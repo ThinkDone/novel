ee5a759b464b158775a175c326101d2c9dfa9286
Thinkdone <thinkdonee@sina.com>
2017-01-24 13:50:45 +0800

    Issue #1: download a complete novel

--- novel/spiders/biqugex.py
+++ novel/spiders/biqugex.py
@@ -11,6 +11,19 @@ class BiqugexSpider(scrapy.Spider):
     start_urls = ['http://biqugex.com/book_26796/11560883.html']
 
     def parse(self, response):
-        with open('xjzm.txt', 'a+') as f:
-            f.write(response.css('title::text').extract_first().encode(response.encoding))
-            
+        chapter_name = response.css('#box_con > div.bookname > h1::text').extract_first().encode(response.encoding)
+
+        with open(chapter_name + '.txt', 'w') as f:
+            f.write(chapter_name)
+            f.write('\n')
+            f.write('{0}\n\n'.format(response.url))
+            for line in response.css('#content ::text').extract():
+                f.write(line.encode(response.encoding))
+        next_chapter_path = response.xpath('//*[@id="box_con"]/div[5]/a[4]/@href').extract_first()
+
+        # the latest chapter
+        if next_chapter_path == '/book_26796/':
+            return
+
+        next_chapter_url = response.urljoin(next_chapter_path)
+        return scrapy.Request(url=next_chapter_url, callback=self.parse)
